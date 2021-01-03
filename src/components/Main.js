05f467197a19c765f115d1090cfdeb351d1cb026
lpercivalDEV <lpercival.dev@gmail.com>
2021-01-03 02:35:22 -0500

    👽 Complete basic api functionality

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,8 +1,18 @@
-import React from "react";
-
-const API_CALL = `api.openweathermap.org/data/2.5/weather?q={city name}&appid=${process.env.DROPLET_APP_OPEN_WEATHER_API_KEY}`;
+import React, { useEffect } from "react";
+import axios from "axios";
 
 const Main = () => {
+  const api_call = async () => {
+    const city_weather = `https://api.openweathermap.org/data/2.5/weather?q=Liverpool&appid=${process.env.REACT_APP_API_KEY}`;
+
+    const request = axios.get(city_weather);
+    const response = await request;
+    console.log(response);
+  };
+  useEffect(() => {
+    api_call();
+  }, []);
+
   return (
     <div className="main">
       <h1>Main Component</h1>
